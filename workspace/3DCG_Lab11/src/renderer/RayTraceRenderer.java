393d81fdfbc53514d900145d7dd981c49d3e2bae
Klaas Van Parijs <warsaalk@gmail.com>
2012-12-12 15:00:32 +0100

    Comment added

--- workspace/3DCG_Lab11/src/renderer/RayTraceRenderer.java
+++ workspace/3DCG_Lab11/src/renderer/RayTraceRenderer.java
@@ -19,8 +19,8 @@ public class RayTraceRenderer extends Renderer {
 	
 	private RayTracer rayTracer;	
 	private int nRows, nCols;	
-	private int ssaan;
-	private float[][] ssaa;
+	private int ssaan; //Number of samples defined by the user + 1 
+	private float[][] ssaa; //See comment constructor
 	
 	public RayTraceRenderer(Scene scene, Camera camera, Properties prop) {
 		super(scene, camera);	
@@ -28,6 +28,9 @@ public class RayTraceRenderer extends Renderer {
 	    nCols = Integer.parseInt(prop.getProperty("canvas.width"));	    
 	    rayTracer = RayTracerFactory.createRayTracer(scene,prop);
 	    ssaan = rayTracer.supersampling+1;
+	    //For each sample we take 2 random numbers between 0 and 0.9999f
+	    // this is a very simple way to create SuperSampling
+	    //see comment render method for usage
 	    ssaa = new float[ssaan][2];
 		for( int i=0; i<ssaan; i++ ){
 			ssaa[i][0] = new Random().nextFloat();
@@ -63,6 +66,15 @@ public class RayTraceRenderer extends Renderer {
 				ArrayList<Colour> colours = new ArrayList<Colour>(ssaan);
 				for( int i=0; i<ssaan; i++ ){
 					Colour temp = new Colour();
+					//As you know we normally just take the color in the top left corner of the "pixel"
+					// that not good enough because the color in the bottom right corner could be totally different.
+					// In the constructor we chose 2 random floats to be added to the current row and column respectively (r and c).
+					// This gives us a random position in the "pixel" and 'always' returns a different color.
+					// Disadvantage, the random positions could be close together and not make a big difference.
+					// Also this makes every render different because it are random numbers.
+					// A better way should be to divide the pixel in grids and 
+					// choose the grids in a way that you maximize the possibility of getting different colors.
+					// Example: if you want 4 samples you take the 4 corners of the pixel
 					float cc = c + ssaa[i][0];
 					float rr = r + ssaa[i][1];
 					ray.dir = new Vector(camera.u.multV(cc*(camera.width/this.nCols)));
@@ -70,8 +82,10 @@ public class RayTraceRenderer extends Renderer {
 					ray.dir.add( v3 );
 					rayTracer.recursionDepth=0;
 					temp.set(rayTracer.shade(ray));
+					//We add the color to a array, which contains the different colors from our sampling
 					colours.add(temp);
 				}
+				//We calculate the average of our array of colors
 				col.average(colours);
 				gl. glColor3f ( col.r, col.g, col.b);
 				gl.glRecti(c, nRows-r, c+1, nRows-r-1);
